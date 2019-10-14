adc0bb420dfcea2e3da2021b1e27a2f4f1d51f61
mtoudert <mtoudert@novaquark.com>
2019-10-14 20:47:23 +0200

    1st lesson

--- CMA/Program.cs
+++ CMA/Program.cs
@@ -10,6 +10,21 @@ namespace CMA
     {
         static void Main(string[] args)
         {
+            int num = 8;
+
+            // Saying hi to CMA
+
+            Console.WriteLine("Bienvenue aux CMA Paris !");
+            Console.WriteLine("My number is : " + num);
+            num = 32;
+            Console.WriteLine("My number is : " + num);
+
+            int a = 1;
+            int result = a++;
+            int result2 = ++a;
+
+            string esc = "trying to \"escape\"";
+
         }
     }
 }
